0b2289580757dd04c61eacd8f9a8a0a52e4702b6
nw117722nw1 <roos1100110022r@gmail.com>
2025-07-19 01:56:36 +0300

    add setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="secure-seed",
+    version="1.0.0",
+    author="Nwal",
+    author_email="roos1100110022r@gmail.com",
+    description="High-entropy seed generator using 10 entropy sources for cryptography and PRNG",
+    url="https://github.com/nw117722nw1/SecureSeed-Python",
+    packages=find_packages(),
+    install_requires=["psutil"],
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
